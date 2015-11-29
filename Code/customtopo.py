a8001eae2c54f6e5ced0a7e42fe75163ad47e626
eserrao <eserrao@ncsu.edu>
2015-11-29 15:52:55 -0800

    Modified the topology

--- Code/customtopo.py
+++ Code/customtopo.py
@@ -24,23 +24,25 @@ class MyTopo( Topo ):
         h1Host = self.addHost( 'h1' )
         h2Host = self.addHost( 'h2' )
         h3Host = self.addHost( 'h3' )
+	h4Host = self.addHost( 'h4' )
         
         s1Switch = self.addSwitch( 's1' )
         s2Switch = self.addSwitch( 's2' )
         s3Switch = self.addSwitch( 's3' )
         s4Switch = self.addSwitch( 's4' )
 
-	linkopts = dict(delay='77ms', bw=1.8 )
-
         # Add links
-        self.addLink( h1Host, s1Switch , **linkopts )
+        self.addLink( h1Host, s1Switch )
         self.addLink( h2Host, s2Switch )
         self.addLink( h3Host, s3Switch )
-        self.addLink( s1Switch, s4Switch )
-        self.addLink( s2Switch, s4Switch )
-        self.addLink( s3Switch, s4Switch )
-        self.addLink( s1Switch, s2Switch )
-        self.addLink( s2Switch, s3Switch )
+	self.addLink( h4Host, s4Switch )
+        self.addLink( s1Switch, s2Switch, delay='1000ms' )
+	self.addLink( s1Switch, s3Switch )
+	self.addLink( s1Switch, s4Switch )        
+	self.addLink( s2Switch, s3Switch )
+	self.addLink( s2Switch, s4Switch )        
+	self.addLink( s3Switch, s4Switch )
+	
         
 topos = { 'mytopo': ( lambda: MyTopo() ) }
 
